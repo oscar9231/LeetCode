5aff5788dc7ffa76c22e032f3d6a30b7af266695
Oscar Li <oslee.cn@gmail.com>
2018-07-14 12:17:38 +0900

    Update [Algorithm] 1. Two Sum.py

--- [Algorithm] 1. Two Sum.py
+++ [Algorithm] 1. Two Sum.py
@@ -5,15 +5,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        list_len = len(nums)
-        i = list_len - 2
-        n = 1
-        while i < list_len - 1 and n < list_len:
-            summa = nums[i] + nums[i + n]
-            if summa == target:
-                return [i, i + n]
-            elif i + n == list_len - 1:
-                i -= 1
-                n = 1
-            else:
-                n += 1
+        i = 0
+        while i < len(nums)-1:
+            j = i + 1
+            while j < len(nums):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                j += 1
+            i += 1
