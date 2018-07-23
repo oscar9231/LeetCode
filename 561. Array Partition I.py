1959a477d75d093f8077fe03078359dc0323f81f
Humanoid Li <oslee.cn@gmail.com>
2018-07-23 15:32:47 +0900

    Create 561. Array Partition I.py

---
+++ 561. Array Partition I.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()　＃sorted() does not return the value
+        sum = 0
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                sum += nums[i]
+        return sum
+        
+# the simplest solution:
+#   def arrayPairSum(self, A):
+      return sum(sorted(A)[::2])
