02a89422cfdbf0094f7446e0aba846eed291b09e
Humanoid Li <oslee.cn@gmail.com>
2018-07-28 17:18:30 +0900

    Update 695. Max Area of Island.py

--- 695. Max Area of Island.py
+++ 695. Max Area of Island.py
@@ -1,15 +1,19 @@
 ＃DFS
 ＃copy from @ELDAN
 
-class Solution:
+class Solution(object):
     def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         n = len(grid)
         m = len(grid[0])
 
         def dfs(x, y):
             if (0 <= x and x < n) and (0 <= y and y < m) and grid[x][y]:
-                grid[x][y] = 0
-                return dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1) + 1
+                grid[x][y] = 0      #prevent duplicate search
+                return dfs(x - 1, y) + dfs(x + 1, y) + dfs(x, y - 1) + dfs(x, y + 1) + 1
             return 0
 
         max_area = 0
@@ -19,3 +23,4 @@ class Solution:
                     max_area = max(max_area, dfs(i, j))
 
         return max_area
+
