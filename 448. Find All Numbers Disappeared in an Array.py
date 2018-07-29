e17ea74e0b98651f99e043ce5b9c3f2fa40ee50e
Humanoid Li <oslee.cn@gmail.com>
2018-07-29 12:09:25 +0900

    Create 448. Find All Numbers Disappeared in an Array.py

---
+++ 448. Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        length = len(nums)
+        nums = list(set(nums))
+        for i in range(1, length + 1):
+            if i in nums:
+                nums.pop(nums.index(i))
+            else:
+                nums.append(i)
+        return nums
