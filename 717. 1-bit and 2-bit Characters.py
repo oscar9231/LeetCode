26125b62ac3ed8742fc1a98e67acfbaac06e8bfb
Humanoid Li <oslee.cn@gmail.com>
2018-07-30 14:51:47 +0900

    Create 717. 1-bit and 2-bit Characters.py

---
+++ 717. 1-bit and 2-bit Characters.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        i = 0
+        while i < len(bits) - 1:
+            if bits[i] == 0:
+                i += 1
+            else:
+                i += 2
+        if i == len(bits):
+            return False
+        else:
+            return True
