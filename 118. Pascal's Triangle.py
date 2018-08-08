cd20891ebaac52efa3d79ad1daa97dc65f3e081f
Humanoid Li <oslee.cn@gmail.com>
2018-08-08 11:39:57 +0900

    Update 118. Pascal's Triangle.py

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        result = [[1]]
+        res = [[1]]
         for i in range(1, numRows):
-            result += [map(lambda x,y: x+y, result[-1] + [0], [0] + result[-1])]
-        return result[:numRows]
+            res.append(list(map(lambda x, y: x + y, [0] + res[-1], res[-1] + [0])))
+        return res[:numRows]
