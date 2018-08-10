b5a50349238cf21f0d7d7b760c79b6b3241d462c
Humanoid Li <oslee.cn@gmail.com>
2018-08-10 13:25:40 +0900

    Update 189. Rotate Array.py

--- 189. Rotate Array.py
+++ 189. Rotate Array.py
@@ -8,3 +8,14 @@ class Solution(object):
         for i in range(k):
             nums.insert(0, nums[-1])
             nums.pop()
+
+            
+# very smart way from programming pearls
+# class Solution:
+    # @param nums, a list of integer
+    # @param k, num of steps
+    # @return nothing, please modify the nums list in-place.
+    # def rotate(self, nums, k):
+       # n = len(nums)
+       # k = k % n
+       # nums[:] = nums[n-k:] + nums[:n-k]
