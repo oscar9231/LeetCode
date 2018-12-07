0be354c08577dc5432e5847e7ab99509b16101a0
Humanoid Li <oslee.cn@gmail.com>
2018-12-07 11:06:15 +0900

    Create 80. Remove Duplicates from Sorted Array II.py

---
+++ 80. Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        i = 0
+        length = len(nums) - 1
+        while i < length:
+            if nums[i] != nums[i+1]:
+                i += 1
+            elif nums[i] == nums[i+1]:
+                print(i+2, length-1)
+                if i+2 <= length and nums[i] == nums[i+2]:
+                    nums.remove(nums[i])
+                    length -= 1
+                else:
+                    i += 2
+        return len(nums)
