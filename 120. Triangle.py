8e6dd95fedec16a1b32a84100b6c5289398c545f
Humanoid Li <oslee.cn@gmail.com>
2018-12-12 15:39:22 +0900

    Create 120. Triangle.py

---
+++ 120. Triangle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        if not triangle:
+            return None
+        i = len(triangle)
+        for row in range(1, i):
+            triangle[row][0] += triangle[row - 1][0]
+            triangle[row][-1] += triangle[row - 1][-1]
+        for row in range(2, i):
+            j = len(triangle[row])
+            for column in range(1, j - 1):
+                triangle[row][column] += min(triangle[row - 1][column - 1], triangle[row - 1][column])
+        return min(triangle[-1])
